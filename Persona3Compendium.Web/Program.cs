ff27bbd6c50c3a3239616d313d9f7c52c622a9aa
JeremyRG32 <Jeremyrg619@gmail.com>
2025-12-06 04:35:34 -0400

    Update Program.cs

--- Persona3Compendium.Web/Program.cs
+++ Persona3Compendium.Web/Program.cs
@@ -8,13 +8,16 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<PersonaCompendiumDbContext>(options =>
     options.UseSqlite("Data Source=Persona.db"));
 
+// >>> ADD THIS <<<
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -30,3 +33,4 @@ app.MapControllerRoute(
     pattern: "{controller=Arcana}/{action=Index}/{id?}");
 
 app.Run();
+
