7d6abd5d97923c0ba63932b932b2606f16a5bb32
JeremyRG32 <Jeremyrg619@gmail.com>
2025-12-06 00:59:59 -0400

    Finished Persona details UI

--- Persona3Compendium.Web/Program.cs
+++ Persona3Compendium.Web/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Arcana}/{action=Index}/{id?}");
 
 app.Run();
